29525a4b0a4bb0830a8b997ccb84c42d943fb31f
Nathami-n <georgenathan010@gmail.com>
2025-02-03 11:51:50 +0300

    form mutations similar to remix only well and better dx

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -4,7 +4,7 @@ import {Form, Link, Outlet} from "react-router";
 import type {Route} from "./+types/sidebar";
 import { getContacts } from "../../app/data";
 
-export async function clientLoader() {
+export async function loader() {
     const contacts = await getContacts();
     return {contacts};
 }
